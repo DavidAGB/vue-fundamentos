5ea94eecbc3ae9725d229dfd1d47051db1b005fc
DavidAGB <davidagbmediagroup@gmail.com>
2022-06-22 16:06:20 -0500

    Pokemon-game-app

---
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>¿Quien es este Pokemon?</h1>
+
+   
+    <PokemonPicture :pokemonId="35" :showPokemon="true"/>
+
+    <PokemonOptions :pokemons="pokemonArr"/>
+{{pokemonArr}}
+ 
+</template>
+
+<script>
+import PokemonOptions from '../components/PokemonOptions.vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
+import getPokemonOptions from  '@/helpers/getPokemonOptions'
+
+ 
+export default {
+    components:{
+        PokemonOptions,
+        PokemonPicture
+    },
+    data() {
+        return {
+            pokemonArr:[],
+        }
+    },
+    mounted() {
+       
+         this.mixPokemonArray()
+         
+    },
+     methods: {
+       mixPokemonArray(){
+      this.pokemonArr =  getPokemonOptions()
+      
+    },
+    
+  }
+
+}
+</script>
+
+<style>
+
+</style>
